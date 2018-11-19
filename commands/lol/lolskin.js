89077c5c0d557a6ebae91fa9d677a0071e92273b
Vegaxys <36893197+Vegaxys@users.noreply.github.com>
2018-11-19 18:15:38 +0100

    27

--- commands/lol/lolskin.js
+++ commands/lol/lolskin.js
@@ -1,6 +1,8 @@
 //**********************    Constantes    ****************************
 const commando = require("discord.js-commando");
 const Discord = require('discord.js');
+const Request = require('request');
+const Cheerio = require('cheerio');
 //**********************       class      ****************************
 class LolSkinCommand extends commando.Command{
   constructor(client){
@@ -19,14 +21,18 @@ class LolSkinCommand extends commando.Command{
 
     var url = `http://ddragon.leagueoflegends.com/cdn/${configbot.ddragon}/data/fr_FR/champion/${champion}.json`;
     var perso = await (await fetch(url)).json();
-    console.log(url);
+
     perso = perso.data[`${champion}`];
     var skins = perso.skins;
     var row = Object.keys(skins).length;
 
     var i;
+    var arrayOfSkins = "";
+    var arrayOfPRrix = "";
+    var skin = bot.emojis.find(emoji => emoji.name === "SKIN");
+
     for (i = 1; i < row; i++) {
-        arrayOfSkins += perso.skins[i].name + '\n';
+        arrayOfSkins += `${skin} `+ perso.skins[i].name + '\n';
       }
     i = 0;
     for (i = 1; i < row; i++) {
@@ -38,8 +44,7 @@ class LolSkinCommand extends commando.Command{
     .setColor("#2e7d32")
     .setTitle(`Liste des skins de ${perso.name}, ${perso.title}`)
     .addField(`Skins`, `${arrayOfSkins}`, true)
-    .addField(`Prix`, `${arrayOfSkins}`, true)
-    .setFooter("Powered by members.luscious.net");
+    .addField(`Prix`, `${arrayOfPRrix}`, true)
     message.channel.send(nudePicture);
   }
 }
