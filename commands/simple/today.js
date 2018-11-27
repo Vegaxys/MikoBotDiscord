507408f6bf81e77f65642d0f40d549339fe0f92f
Vegaxys <36893197+Vegaxys@users.noreply.github.com>
2018-11-27 17:59:47 +0100

    32

--- commands/simple/today.js
+++ commands/simple/today.js
@@ -16,10 +16,8 @@ class TodayCommand extends commando.Command{
 
   async run(message, args){
     //v!today
-    var todayJSON = {
-      "date": "today",
-      "fete": "",
-      "citation": ""
+    if(message.member.id != bot.owners[0].id){
+      return message.channel.send(`désolé, mais seul ${bot.owners[0].username} peut faire çela`);
     }
     request('https://operabaroque.fr/fete-jour.htm', function(error, response, html){
       var $ = cheerio.load(html);
@@ -29,32 +27,38 @@ class TodayCommand extends commando.Command{
         console.log(response.statusCode);
       }
 
-      var longstring = $('h2').next().text();
-      //***************************************** Date */
-      var longDate  = longstring.slice(15);
-      var date = longDate.split(' ').filter( word => word.length > 0);
-      longDate = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
+    var todayJSON = {
+      "date": "today",
+      "fete": "",
+      "citation": ""
+    }
+    
+    var longstring = $('h2').next().text();
+    //***************************************** Date */
+    var longDate  = longstring.slice(15);
+    var date = longDate.split(' ').filter( word => word.length > 0);
+    longDate = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
 
-      //***************************************** Saints */
-      var maxWord = longDate.length;
-      var debutSaint = date.indexOf('saint(s)') + 2;
-      var finSaint = date.indexOf('.\nVous');
-      var saints = "";
-      for (let i = debutSaint; i < finSaint; i++) {
-        saints += date[i] + ", ";
-        
-      }
+    //***************************************** Saints */
+    var maxWord = longDate.length;
+    var debutSaint = date.indexOf('saint(s)') + 2;
+    var finSaint = date.indexOf('.\nVous');
+    var saints = "";
+    for (let i = debutSaint; i < finSaint; i++) {
+      saints += date[i] + ", ";
       
-      todayJSON.date = longDate;
-      todayJSON.fete = saints;
+    }
+    
+    todayJSON.date = longDate;
+    todayJSON.fete = saints;
 
-      let levelEmbed = new Discord.RichEmbed()
-      .setAuthor(`Miko | 巫女`, bot.user.avatarURL)
-      .setTitle(":date: Fête du jour :date: ")
-      .setColor("#2e7d32")
-      .addField(`${todayJSON.date}`, `Bonjour à tous et à toutes, aujourd'hui nous célébrons les ${todayJSON.fete}. 
-      Vous pouvez donc leurs souhaiter une bonne fête et une bonne journée !`)
-    message.channel.send(levelEmbed);
+    let todayEmbed = new Discord.RichEmbed()
+    .setAuthor(`Miko | 巫女`, bot.user.avatarURL)
+    .setTitle(":date: Fête du jour :date: ")
+    .setColor("#2e7d32")
+    .addField(`${todayJSON.date}`, `Bonjour à tous et à toutes, aujourd'hui nous célébrons les **${todayJSON.fete}** 
+    vous pouvez donc leurs souhaiter une bonne fête et une bonne journée !`)
+    message.channel.send(todayEmbed);
     });
   }
 }
